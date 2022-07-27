40f8c7f08bbf5f32cc41369cef5350ea7d1cf9ed
LedesmaMateo <mateo.ledesma@hotmail.com>
2022-07-27 01:18:42 -0300

    c2

--- src/main/java/com/example/argentinaprograma/proyectofinal/ProyectoFinalApplication.java
+++ src/main/java/com/example/argentinaprograma/proyectofinal/ProyectoFinalApplication.java
@@ -2,6 +2,9 @@ package com.example.argentinaprograma.proyectofinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectoFinalApplication {
@@ -9,5 +12,15 @@ public class ProyectoFinalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoFinalApplication.class, args);
 	}
+        
+        @Bean
+        public WebMvcConfigurer getCorsConfiguration(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/api/**").allowedOrigins("https://argentinaprograma-58a08.web.ap");
+            }
+        };
+    }
 
 }
