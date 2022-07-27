d530a29fa97115dcaf9c6226e5cd8c11d868746b
LedesmaMateo <mateo.ledesma@hotmail.com>
2022-07-27 13:53:16 -0300

    c2

--- src/main/java/com/example/argentinaprograma/proyectofinal/configuration/SecurityConfig.java
+++ src/main/java/com/example/argentinaprograma/proyectofinal/configuration/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-            http
+        http.csrf().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and()
