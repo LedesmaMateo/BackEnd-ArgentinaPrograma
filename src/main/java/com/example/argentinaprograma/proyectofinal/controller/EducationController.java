97521888bbe798167f99068efa440257ee2d393c
LedesmaMateo <mateo.ledesma@hotmail.com>
2022-07-26 23:36:35 -0300

    c2

--- src/main/java/com/example/argentinaprograma/proyectofinal/controller/EducationController.java
+++ src/main/java/com/example/argentinaprograma/proyectofinal/controller/EducationController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://argentinaprograma-58a08.web.app")
+@CrossOrigin(origins = {"http://localhost:4200", "https://argentinaprograma-58a08.web.app"})
 @RequestMapping("/api/education")
 public class EducationController {
     @Autowired
--- src/main/java/com/example/argentinaprograma/proyectofinal/controller/ProfileController.java
+++ src/main/java/com/example/argentinaprograma/proyectofinal/controller/ProfileController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://argentinaprograma-58a08.web.app")
+@CrossOrigin(origins = {"http://localhost:4200", "https://argentinaprograma-58a08.web.app"})
 @RequestMapping("/api/profile")
 public class ProfileController {
     @Autowired
