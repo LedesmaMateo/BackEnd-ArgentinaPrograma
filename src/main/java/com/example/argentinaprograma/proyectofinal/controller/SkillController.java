bf9a253a6e37998796d7cfe0be3ade674f0c2999
LedesmaMateo <mateo.ledesma@hotmail.com>
2022-07-27 14:08:11 -0300

    c2

--- src/main/java/com/example/argentinaprograma/proyectofinal/controller/SkillController.java
+++ src/main/java/com/example/argentinaprograma/proyectofinal/controller/SkillController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@CrossOrigin(origins = "https://argentinaprograma-58a08.web.app")
 @RestController
 @RequestMapping("/api/skill")
 public class SkillController {
